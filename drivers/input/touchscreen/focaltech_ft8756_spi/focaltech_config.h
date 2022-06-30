333709ace71daa5ed45341945e3ffc7c2a54e1fa
TheMalachite <eliasgheeraert@gmail.com>
2022-07-01 01:52:59 +0200

    input: touchscreen: focaltech_ft8756_spi: Switch to dummy fw file for FTS_UPGRADE_FW_FILE
     * FT8756_V30_D00_20201225_app.i isn't present anywhere

--- drivers/input/touchscreen/focaltech_ft8756_spi/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_ft8756_spi/focaltech_config.h
@@ -255,7 +255,7 @@
  * define your own fw_file, the sample one to be replaced is invalid
  * NOTE: if FTS_GET_MODULE_NUM > 1, it's the fw corresponding with FTS_VENDOR_ID
  */
-#define FTS_UPGRADE_FW_FILE                      "include/firmware/FT8756_V30_D00_20201225_app.i"
+#define FTS_UPGRADE_FW_FILE                      "include/firmware/fw_sample.h"
 
 /*
  * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_VENDOR_ID2
